3831e3d333caa13080e154ecaa69b55c7f0896af
Lucien <llucangioli@gmail.com>
2023-05-17 16:33:32 -0300

    hasta el search

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,13 +5,13 @@ const Login = () => {
         <div>
           <form>
   <div className="mb-3">
-    <label for="exampleInputEmail1" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <label for="exampleInputEmail1" className="form-label">Email</label>
+    <input type="email" className="form-control1" id="exampleInputEmail1" aria-describedby="emailHelp"/>
     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div className="mb-3">
     <label for="exampleInputPassword1" className="form-label">Password</label>
-    <input type="password" className="form-control" id="exampleInputPassword1"/>
+    <input type="password" className="form-control1" id="exampleInputPassword1"/>
   </div>
   <div className="mb-3 form-check">
     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
